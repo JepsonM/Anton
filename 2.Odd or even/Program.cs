5bc1c0f3528d98d5d455fbed23b26178a145ebca
JepsonM <sinkfact@gmail.com>
2016-06-09 02:05:45 -0700

    Odd or Even

---
+++ 2.Odd or even/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        int number;
+        Console.WriteLine("Give me the number ");
+        number = Convert.ToInt32(Console.ReadLine());
+
+            
+
+            if (number % 2 == 0)
+            Console.WriteLine("This number is even ");
+
+            else
+                
+            Console.WriteLine("This number is odd ");
+        }
+    }
+
