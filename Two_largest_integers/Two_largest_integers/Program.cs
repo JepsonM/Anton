3517633ea086382bec21f2c29d761c483e036450
JepsonM <sinkfact@gmail.com>
2016-06-09 02:31:53 -0700

    Two largest integers
    Find the largest and second largest integers among 10 integers which are
    given by the user

---
+++ Two_largest_integers/Two_largest_integers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Two_largest_integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number; 
+            Console.Write("Enter the integer ");
+            number = Convert.ToInt32(Console.ReadLine()); // integers as an input
+            int largest = int.MinValue; //refreshing the largest value
+            int second_largest = int.MinValue; //refreshing the second largest value
+
+            for (int i = 0; i < 10; i++) // 10 times loop
+            {
+                if (number > largest) //for the largest integer
+                {
+                    largest = number;
+                }
+                else if (number > second_largest && largest > second_largest) //two conditions for second largest
+                {
+                    second_largest = number;
+                }
+            }
+            Console.WriteLine("The largest integer is {0}", largest);
+            Console.WriteLine("The second largest integer is {0}", second_largest);
+        }
+    }
+}
