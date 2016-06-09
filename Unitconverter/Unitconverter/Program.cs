9c19bb5e167d867312c2c0249268897021391c8a
JepsonM <sinkfact@gmail.com>
2016-06-09 02:42:54 -0700

    Unitconverter

---
+++ Unitconverter/Unitconverter/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unitconverter
+{
+    class UnitConverter
+        {        
+            int ratio;
+            public UnitConverter (int unitRatio){ ratio = unitRatio; }
+            public int Convert(int unit){ return unit * ratio; }
+        }
+
+    class Test
+    {
+        static void Main()
+        {
+            UnitConverter feetToInchesConverter = new UnitConverter(12);
+            UnitConverter milesToFeetConverter = new UnitConverter(5280);
+
+            Console.WriteLine(feetToInchesConverter.Convert(30));
+            Console.WriteLine(feetToInchesConverter.Convert(100));
+            Console.WriteLine(feetToInchesConverter.Convert(milesToFeetConverter.Convert(1)));
+        }
+    }
+
+        
+}
