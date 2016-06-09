f0635e7195fdfc9267cac0b78dc5f3b15947bf7b
JepsonM <sinkfact@gmail.com>
2016-06-09 02:03:14 -0700

    Shapes

---
+++ 1.Shapes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace ConsoleApplication1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("*********" + "    ***   " + "   *   " + "    *    ");
+            Console.WriteLine("*       *" + "  *     * " + "  ***  " + "   * *   ");
+            Console.WriteLine("*       *" + " *       *" + " ***** " + "  *   *  ");
+            Console.WriteLine("*       *" + " *       *" + "   *   " + " *     * ");
+            Console.WriteLine("*       *" + " *       *" + "   *   " + "*       *");
+            Console.WriteLine("*       *" + " *       *" + "   *   " + " *     * ");
+            Console.WriteLine("*       *" + " *       *" + "   *   " + "  *   *  ");
+            Console.WriteLine("*       *" + "  *     * " + "   *   " + "   * *   ");
+            Console.WriteLine("*********" + "    ***   " + "   *   " + "    *    ");
+
+        }
+    }
+}
