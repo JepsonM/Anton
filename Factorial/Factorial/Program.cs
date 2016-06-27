b7b3237db8fce27f6e00cddea2f6d18b71e52ef0
JepsonM <sinkfact@gmail.com>
2016-06-27 14:58:47 -0700

    Factorial

---
+++ Factorial/Factorial/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class Program
+    {
+        static int Factorial(int num)
+        {
+            if (num == 1)
+            {
+                return 1;
+            }
+            if (num == 0)
+            {
+                return 1;
+            }
+            return num * Factorial(num - 1);
+        }
+        static void Main(string[] args)
+        {             
+            Console.Write("Enter a non-negative integer ");
+            int number;
+            number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("{0}! is {1}", number, Factorial(number));
+        }
+    }
+}
