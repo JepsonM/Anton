1686494f36abc6f3461cbfe6094b4e7de98a8ceb
JepsonM <sinkfact@gmail.com>
2016-06-27 19:23:58 -0700

    Basic Drawing(Pattern)
    Loop is used

---
+++ Checkerboard_Pattern_of_Asterisks/Checkerboard_Pattern_of_Asterisks/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkerboard_Pattern_of_Asterisks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter1 = 1; //Big loop counter
+            int counter2 = 1; //Small loop counter
+
+            while (counter1 <= 8) //Loop for \n
+            {
+                if (counter1 % 2 != 0) //For odd number rows
+                {
+                    while (counter2 <= 8) 
+                    {
+                        Console.Write("* ");
+                        counter2++;
+                    }
+                }
+                else //For even number rows, insert a space
+                {
+                    Console.Write(" "); //Single space insert
+                    while (counter2 <= 8)
+                    {
+                        Console.Write("* ");
+                        counter2++;
+                    }
+                }
+                counter2 = 1;
+                counter1++;
+                Console.WriteLine();
+            }
+        }
+    }
+}
